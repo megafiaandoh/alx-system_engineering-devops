a3858b758145583da7892f87453eb5028392e960
megafiaandoh <andoh26@gmail.com>
2024-04-19 16:03:27 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-#create a file in /tmp.
+#Create a file in /tmp
 
-file { '/tmp/holberton':
+file { 'school':
 ensure  => 'present',
+path    => '/tmp/school',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
-content => 'I love Puppet',}
+content => 'I love Puppet'
+}
