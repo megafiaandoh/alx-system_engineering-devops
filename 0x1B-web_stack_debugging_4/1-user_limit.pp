d083fa4081039f0fb11e1d5bd0e498b3f515987e
megafiaandoh <andoh26@gmail.com>
2024-06-13 14:13:36 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,2 +1,7 @@
-# Changes the OS configuration so that it is possible to login with the holberton user and open a file without any error message
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
